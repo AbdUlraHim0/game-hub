7d03166b4ef99b100127c528243f90d869e9ab85
AbdUlraHim0 <Abdulrahim@githup.com>
2023-11-27 20:35:23 +0400

    refactor - manage state with zistand

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -7,16 +7,14 @@ import {
   ListItem,
   Spinner,
 } from "@chakra-ui/react";
-import useGenres, { Genre } from "../hooks/useGenres";
+import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import useGameQueryStore from "../store";
 
-interface Props {
-  onSelectGenre: (genre: Genre) => void;
-  selectedGerneId?: number;
-}
-
-const GenreList = ({ selectedGerneId, onSelectGenre }: Props) => {
+const GenreList = () => {
   const { data, isLoading, error } = useGenres();
+  const selectedGerneId = useGameQueryStore((s) => s.gameQuery.genreId);
+  const setSelectedGenreId = useGameQueryStore((s) => s.setGenreId);
 
   if (error) return null;
   if (isLoading) return <Spinner />;
@@ -41,7 +39,7 @@ const GenreList = ({ selectedGerneId, onSelectGenre }: Props) => {
                 textAlign="left"
                 fontSize="lg"
                 variant="link"
-                onClick={() => onSelectGenre(genre)}
+                onClick={() => setSelectedGenreId(genre.id)}
                 fontWeight={genre.id === selectedGerneId ? "bold" : "normal"}
               >
                 {genre.name}
